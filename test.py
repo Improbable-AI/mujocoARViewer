fe68ad7ddef67e6c6186869e192dde34aad88c0a
younghyopark <younghyo@mit.edu>
2025-11-11 13:59:44 -0500

    update

--- test.py
+++ test.py
@@ -1,17 +1,15 @@
 import mujoco 
-from mujoco_ar_viewer import MJARViewer
+from mujoco_ar_viewer import mujocoARViewer
 import mujoco.viewer
 
 xml_path = "scenes/franka_emika_panda/scene.xml"
-usdz_path = "scenes/franka_emika_panda/scene.usdz"
 
 model = mujoco.MjModel.from_xml_path(xml_path)
 data = mujoco.MjData(model)
 
 avp_ip = "192.168.86.20"
-arviewer = MJARViewer(avp_ip=avp_ip, grpc_port=50051, enable_hand_tracking=False)
-arviewer.load_scene(usdz_path, attach_to=[0, 1, 0.0, 1, 0, 0, 0])  # Load scene 0.5m up
-
+arviewer = mujocoARViewer(avp_ip=avp_ip, grpc_port=50051, enable_hand_tracking=False)
+arviewer.load_scene(xml_path, attach_to=[0, 1, 0.0, 1, 0, 0, 0])  # Load scene 0.5m up
 arviewer.register(model, data)
 
 viewer = mujoco.viewer.launch_passive(model, data)
